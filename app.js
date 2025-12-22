714b73466650b0eef32d8d7d12ea60e1e6a97473
harshitislive <codewithharshit7live@gmail.com>
2025-12-22 21:21:12 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+var audio = document.getElementById("myAudio");
+var pic = document.getElementById('pic');
+
+function startDance() {
+    pic.src = 'dance.gif';
+    audio.play();
+}
+
+
+function stopDance() {
+    pic.src = 'stand.webp';
+    audio.pause();
+}
